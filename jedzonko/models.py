2f09d7085d136cc9087fee0ea9b226e320f74026
Lukasz Krysztofinski <lkrysz@gmail.com>
2019-02-19 08:17:20 +0100

    Metody views nie przyjmuja argumentow?

--- jedzonko/models.py
+++ jedzonko/models.py
@@ -1,4 +1,54 @@
 from django.db import models
+import psycopg2
+import pytz
+
+from django.db import models
+
+days = (
+    (0, 'Poniedzialek'),
+    (1, 'Wtorek'),
+    (2, 'Sroda'),
+    (3, 'Czwartek'),
+    (4, 'Piatek'),
+    (5, 'Sobota'),
+    (6, 'Niedziela'),
+)
 
 
-# Create your models here.
+class JedzonkoRecipe(models.Model):
+    name = models.CharField(max_length=255)
+    ingredients = models.TextField()
+    description = models.TextField(null=True)
+    created = models.TimeField(auto_now_add=True)
+    updated = models.TimeField(auto_now_add=True)
+    preparation_time = models.IntegerField()
+    votes = models.IntegerField(null=True)
+
+    def __str__(self):
+        return f'{self.id},{self.name},{self.ingredients},{self.description}, {self.created}, {self.updated},' \
+            f'{self.preparation_time},{self.votes}'
+
+
+class JedzonkoPlan(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True)
+    created = models.TimeField(auto_created=True)
+
+
+class JedzonkoDayname(models.Model):
+    day_name = models.IntegerField(choices=days)
+    order = models.IntegerField(null=True)
+
+
+class JedzonkoRecipeplan(models.Model):
+    meal_name = models.CharField(max_length=255)
+    order = models.IntegerField()
+    day_name_id = models.ForeignKey(JedzonkoDayname, on_delete=models.DO_NOTHING)
+    plan_id = models.ForeignKey(JedzonkoPlan, on_delete=models.DO_NOTHING)
+    recipe_id = models.ForeignKey(JedzonkoRecipe, on_delete=models.DO_NOTHING)
+
+
+class JedzonkoPage(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    slug = models.CharField(max_length=255)
--- jedzonko/views.py
+++ jedzonko/views.py
@@ -2,6 +2,8 @@ from datetime import datetime
 
 from django.shortcuts import render
 from django.views import View
+from jedzonko.models import JedzonkoRecipe
+import random
 
 
 class IndexView(View):
@@ -9,3 +11,29 @@ class IndexView(View):
     def get(self, request):
         ctx = {"actual_date": datetime.now()}
         return render(request, "test.html", ctx)
+
+
+class Random:
+    lista = []
+
+    def list_it(self):
+        var = JedzonkoRecipe.objects.all()
+        for v in var:
+            self.lista.append(v.id)
+        return self.lista
+
+    def random_it(self):
+        choose = random.sample(self.lista, 3)
+        return choose
+
+    def show_must_go_on(self, request, choose):
+        if request.method == 'GET':
+            x = choose[0]
+            show_it = JedzonkoRecipe.objects.get(id=int(x))
+            y = {'losuj': show_it.description}, {'title': show_it.name}
+            return render(request, 'index.html', y)
+        else:
+            x = choose[1]
+            show_it = JedzonkoRecipe.objects.get(id=int(x))
+            y = {'losuj': show_it.description}, {'title': show_it.name}
+            return render(request, 'index.html', y)
