99a7f6dc34c8f9c8d15da94c3bc8f3c5b2d0635f
MaciejKwiecinski <mackwiecinski@gmail.com>
2019-02-20 00:22:55 +0100

    test2

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-
+from jedzonko.models import jedzonko_dayname,jedzonko_recipe,jedzonko_plan,jedzonko_page,jedzonko_recipeplan
 from django.shortcuts import render
 from django.views import View
 
@@ -9,3 +9,25 @@ class IndexView(View):
     def get(self, request):
         ctx = {"actual_date": datetime.now()}
         return render(request, "test.html", ctx)
+
+def main(request):
+
+def land(request):
+
+def recipe(request):
+
+def recipe_list(request):
+
+def add_recipe(request):
+
+def edit_recipe(request):
+
+def plan(request):
+
+def add_plan(request):
+
+def add_details_plan(request):
+
+def contact(request):
+
+def about(request):
